f63333f762f2c5fdb20474fb51b54bd9a2a3a67a
Dmitry <aquamore@outlook.com>
2014-05-22 19:34:32 +0300

    finished fix bugs for started on Tomcat server

--- src/main/java/ua/org/project/web/controller/ContactController.java
+++ src/main/java/ua/org/project/web/controller/ContactController.java
@@ -1,16 +1,19 @@
 package ua.org.project.web.controller;
 
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ua.org.project.domain.Contact;
 import ua.org.project.service.ContactService;
+import ua.org.project.web.form.ContactGrid;
 
 import java.util.List;
 
@@ -43,5 +46,58 @@ public class ContactController {
         Contact contact = contactService.findById(id);
         uiModel.addAttribute("contact", contact);
         return "contacts/show";
+    }
+
+    @RequestMapping(value = "/listgrid", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ContactGrid listGrid(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "rows", required = false) Integer rows,
+            @RequestParam(value = "sidx", required = false) String sortBy,
+            @RequestParam(value = "sord", required = false) String order
+    ) {
+        logger.info("Get ContactGrid with params: page {}, rows: {},", page, rows);
+        logger.info("Next params: sort: {}, order: {}", sortBy, order);
+
+        Sort sort = null;
+        String orderBy = sortBy;
+        if (orderBy != null && orderBy.equals("birthDateString"))
+            orderBy = "birthDate";
+
+        if (orderBy != null && order != null) {
+            if (order.equals("desc")) {
+                sort = new Sort(Sort.Direction.DESC, orderBy);
+            } else {
+                sort = new Sort(Sort.Direction.ASC, orderBy);
+            }
+        }
+
+        PageRequest pageRequest = null;
+
+        if (sort != null) {
+            pageRequest = new PageRequest(page-1, rows, sort);
+        } else {
+            pageRequest = new PageRequest(page-1, rows);
+        }
+
+        Page<Contact> contactPage = contactService.findAllByPage(pageRequest);
+        ContactGrid contactGrid = new ContactGrid();
+
+        contactGrid.setCurrentPage(contactPage.getNumber() + 1);
+        contactGrid.setTotalPages(contactPage.getTotalPages());
+        contactGrid.setTotalRecords(contactPage.getTotalElements());
+        contactGrid.setContactData(Lists.newArrayList(contactPage.iterator()));
+
+        return contactGrid;
+    }
+
+    @RequestMapping(value = "/photo/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public byte[] downloadPhoto(@PathVariable("id") Long id) {
+        Contact contact = contactService.findById(id);
+        if (contact.getPhoto() != null){
+            logger.info("Download photo id: {}, size {}", contact.getId(), contact.getPhoto().length);
+        }
+        return contact.getPhoto();
     }
 }
